ee136cef24ae87dfcfe05dd4ee603b66168dddea
CarlosCosmo <40824256+CarlosCosmo@users.noreply.github.com>
2021-06-24 12:35:36 +0200

    Another event update

--- src/Event/ConfigureEvent.php
+++ src/Event/ConfigureEvent.php
@@ -15,7 +15,7 @@ namespace Sonata\AdminBundle\Event;
 
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Mapper\MapperInterface;
-use Symfony\Contracts\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\Event;
 
 /**
  * This event is sent by hook:
